d179bf94f528753830f6387bd285ee3352f493ed
youngsoosoo <lovepys01@naver.com>
2022-06-14 02:46:08 +0900

    2022-06-14_구글 지도 API 생성

--- app/src/main/java/com/inhatc/vegan_android/MainActivity.java
+++ app/src/main/java/com/inhatc/vegan_android/MainActivity.java
@@ -2,14 +2,40 @@ package com.inhatc.vegan_android;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button Bake;
+    private Button Rest;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Bake = (Button) findViewById(R.id.Bake);
+        Bake.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // SignUpActivity 연결
+                Intent intent = new Intent(MainActivity.this, MapsActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        Rest = (Button) findViewById(R.id.Rest);
+        Rest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // SignUpActivity 연결
+                Intent intent = new Intent(MainActivity.this, MapsActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
